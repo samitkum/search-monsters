f6ced17be1038614ff2a21cccdac577657d7afe6
Singh <samitkum@netapp.com>
2020-01-31 13:21:39 +0530

    Updates

---
+++ precache-manifest.14a770ee21d28d1b02e82793251b8900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15efb75ff418dd0abf9febbb0f6ec0a",
+    "url": "/search-monsters/index.html"
+  },
+  {
+    "revision": "da8a5fe8e7c215babecd",
+    "url": "/search-monsters/static/css/main.4891cd5a.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/search-monsters/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/search-monsters/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da8a5fe8e7c215babecd",
+    "url": "/search-monsters/static/js/main.af19a886.chunk.js"
+  },
+  {
+    "revision": "ba03617a9f6bc29bf9aa",
+    "url": "/search-monsters/static/js/runtime-main.57115506.js"
+  }
+]);
